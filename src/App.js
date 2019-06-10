ac56a7c9dacb3486631476da5d6914510ef3033d
deeem <karpov.dimon@gmail.com>
2019-06-10 15:04:49 +0300

    remove app.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import "./App.css";
 import Layout from "./components/Layout/Layout";
 import Dashboard from "./pages/Dashboard";
 import Bits from "./pages/Bits";
