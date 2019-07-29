33a0f7cd9256d9d56995200a6bbfe4fb61c3ecaf
deeem <karpov.dimon@gmail.com>
2019-07-29 11:27:34 +0300

    auto login

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Layout from './components/Layout/Layout'
 import Dashboard from './pages/Dashboard'
@@ -7,20 +7,43 @@ import Games from './pages/Games'
 import Users from './pages/Users'
 import GameEdit from './pages/GameEdit'
 import Login from './pages/Login'
+import { connect } from 'react-redux'
+import * as actions from './store/actions'
 
-function App() {
-  return (
-    <Switch>
-      <Route path="/login" component={Login} />
-      <Layout>
-        <Route path="/bits" component={Bits} />
-        <Route path="/games/:id" component={GameEdit} />
-        <Route path="/games" component={Games} />
-        <Route path="/users" component={Users} />
-        <Route path="/" exact component={Dashboard} />
-      </Layout>
-    </Switch>
-  )
+class App extends Component {
+  componentDidMount() {
+    this.props.onTryAutoSingup()
+  }
+
+  render() {
+    return (
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Layout>
+          <Route path="/bits" component={Bits} />
+          <Route path="/games/:id" component={GameEdit} />
+          <Route path="/games" component={Games} />
+          <Route path="/users" component={Users} />
+          <Route path="/" exact component={Dashboard} />
+        </Layout>
+      </Switch>
+    )
+  }
 }
 
-export default App
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !== null,
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onTryAutoSingup: () => dispatch(actions.authCheckState()),
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(App)
