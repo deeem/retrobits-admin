678b92adc2f465ee748033ae22501f65497f6859
deeem <karpov.dimon@gmail.com>
2019-07-29 13:02:12 +0300

    logout functionality

--- src/components/Layout/NavBar.js
+++ src/components/Layout/NavBar.js
@@ -1,41 +1,66 @@
-import React from "react";
-import { NavLink, withRouter } from "react-router-dom";
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { NavLink, withRouter } from 'react-router-dom'
 import {
   Dashboard as DashboardIcon,
   VideogameAsset as VideoGameAssetIcon,
   OndemandVideo as OndemandVideoIcon,
-  AssignmentInd as AssignmentIndIcon
-} from "@material-ui/icons";
+  AssignmentInd as AssignmentIndIcon,
+  ExitToApp as ExitToAppIcon,
+} from '@material-ui/icons'
 import {
   ListItemIcon,
   ListItemText,
   MenuItem,
-  MenuList
-} from "@material-ui/core";
+  MenuList,
+  Divider,
+} from '@material-ui/core'
+import * as actions from '../../store/actions'
 
 const NavBar = props => {
+  const token = useSelector(state => state.auth.token)
+  const dispatch = useDispatch()
+
+  const redirectToLogin = () => {
+    dispatch(actions.logout())
+    props.history.push('/login')
+  }
+
   const DashboardLink = React.forwardRef((props, ref) => (
     <NavLink to="/" {...props} innerRef={ref} />
-  ));
+  ))
 
   const BitsLink = React.forwardRef((props, ref) => (
     <NavLink to="/bits" {...props} innerRef={ref} />
-  ));
+  ))
 
   const GamesLink = React.forwardRef((props, ref) => (
     <NavLink to="/games" {...props} innerRef={ref} />
-  ));
+  ))
 
   const UsersLink = React.forwardRef((props, ref) => (
     <NavLink to="/users" {...props} innerRef={ref} />
-  ));
+  ))
+
+  const ExitLink = token ? (
+    <>
+      <Divider />
+
+      <MenuItem button onClick={() => redirectToLogin()}>
+        <ListItemIcon>
+          <ExitToAppIcon />
+        </ListItemIcon>
+        <ListItemText primary="Exit" />
+      </MenuItem>
+    </>
+  ) : null
 
   return (
     <MenuList>
       <MenuItem
         button
         component={DashboardLink}
-        selected={"/" === props.location.pathname}
+        selected={'/' === props.location.pathname}
       >
         <ListItemIcon>
           <DashboardIcon />
@@ -46,7 +71,7 @@ const NavBar = props => {
       <MenuItem
         button
         component={BitsLink}
-        selected={"/bits" === props.location.pathname}
+        selected={'/bits' === props.location.pathname}
       >
         <ListItemIcon>
           <OndemandVideoIcon />
@@ -57,7 +82,7 @@ const NavBar = props => {
       <MenuItem
         button
         component={GamesLink}
-        selected={"/games" === props.location.pathname}
+        selected={'/games' === props.location.pathname}
       >
         <ListItemIcon>
           <VideoGameAssetIcon />
@@ -68,15 +93,17 @@ const NavBar = props => {
       <MenuItem
         button
         component={UsersLink}
-        selected={"/users" === props.location.pathname}
+        selected={'/users' === props.location.pathname}
       >
         <ListItemIcon>
           <AssignmentIndIcon />
         </ListItemIcon>
         <ListItemText primary="Users" />
       </MenuItem>
-    </MenuList>
-  );
-};
 
-export default withRouter(NavBar);
+      {ExitLink}
+    </MenuList>
+  )
+}
+
+export default withRouter(NavBar)
